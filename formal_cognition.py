7be3c704104256878704ec75d77391e1b21fc05e
David Shapiro <daveshap37@gmail.com>
2018-06-10 20:57:19 -0400

    adding inline ideas and guidance

--- formal_cognition.py
+++ formal_cognition.py
@@ -1,59 +1,66 @@
 import requests
 import time
 
-top_level_imperatives = [
+core_constitution = [
     'avoid causing harm or damage',
     'learn about the world, be curious',
     'be empathetic towards all life',
+    'solve scientific problems',
 ]
 
 
 def examine_recent_history():
-    # query chronology and stream of consciousness
-    results = []
-    return results
+    """the purpose of examine recent history is to build a complete context
+    a context is a set of statements and records placing the mind in time and space
+    the context includes statements about locations, states of being, interactions, thoughts, and more
+    context statements may be as simple as 'i am outside'
+    the sum total of context statements should create a complete picture of the machine's current experience"""
+    context = []
+    return context
 
 
-def expound_candidate(cand):
-    # enumerate possibilities TODO
-    results = []
-    return results
+def generate_thoughts(context):
+    """thoughts are a running commentary on the context
+    thoughts seek to analyze the context, posing questions and/or making observations
+    in both cases, thoughts pull in the larger context of knowledge and experience
+    this simply means that items in the context are used to query the KB for more information
+    in this way, a thought is the pairing of context and experience"""
+    thoughts = []
+    return thoughts
 
 
-def identify_tasks_or_directives(history):
-    candidates = []
-    for item in history:
-        candidates.append(expound_candidate(item))
-    results = []
-    for item in candidates:
+def develop_ideas(thoughts):
+    """an idea is a formal thought
+    thoughts are casual and abstract, and may be completely meaningless
+    ideas are thoughts taken out to logical conclusions
+    whereby the validity or impact of a thought can be measured
+    for instance, if a thought of interacting with poison comes to mind
+    one logical result (prognosis) could be causing harm or death
+    this would violate the core constitution and thus be rated as having negative value"""
+    ideas = []
+    for item in thoughts:
         corpus = compile_corpus(item)  # TODO
         prognosis = anticipate_results(corpus)  # TODO
-        value = evaluate_task_results(corpus, prognosis)  # TODO
-        results.append({'corpus': corpus, 'prognosis': prognosis, 'value': value})
-    return results
+        value = evaluate_prognosis(corpus, prognosis)  # TODO
+        ideas.append({'corpus': corpus, 'prognosis': prognosis, 'value': value})
+    return ideas
 
 
-def filter_and_prioritize(history, tasks):
-    # weigh tasks and prognoses against imperatives
-    # generate relativistic score for each imperative against each task
-    # sort and filter
+def filter_and_prioritize(history, ideas):
+    """filtering and prioritizing are the second major component of executive cognitive reasoning
+    while ideas are weighed and analyzed within the mind, tasks are ideas given intention and conviction
+    this is the point at which ideas are translated into tasks
+    to carry the example of using poison forward, with the negative value, the task would be 'do not use poison'
+    this might translate to an action 'put poison away' or merely a decision to vacate the thought"""
     results = []
     return results
-
-
-def execute_tasks(tasks):
-    # record decisions in stream of consciousness
-    # for each task, clarify actions and goals (summarize, simplify, etc)
-    # initiate tasks via REST, or take no action to let existing tasks complete
-    # note: tasks will update the kb service
-    print('bacon')
 
 
 if __name__ == '__main__':
-    my_current_state = top_level_imperatives[0]
     while True:
-        history = examine_recent_history()  # TODO
-        tasks = identify_tasks_or_directives(history)  # TODO
-        tasks = filter_and_prioritize(history, tasks)  # TODO
-        execute_tasks(tasks)  # TODO
-        time.sleep(30)
+        context = examine_recent_history()  # TODO
+        thoughts = generate_thoughts(context)
+        ideas = develop_ideas(thoughts)  # TODO
+        tasks = filter_and_prioritize(context, ideas)  # TODO
+        plans = develop_plans(tasks)
+        execute_tasks(plans)
